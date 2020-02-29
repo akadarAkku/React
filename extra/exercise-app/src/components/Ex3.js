4db2a09590a9d7a18dbf04bc088c6291fccaf192
Akadar.akku <akadar.akku121@gmail.com>
2020-02-29 18:17:58 +0100

    Extra Exercise is done

--- extra/exercise-app/src/components/Ex3.js
+++ extra/exercise-app/src/components/Ex3.js
@@ -1,33 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 
 const Ex3 = () => {
   const [count, setCount] = useState(0);
-
-  useEffect((num ) => {
-    // Update the document title using the browser API
-    if (num % 15 === 0) {
-    return "fizzbuzz";
+ return (
+      <div>
+        {count % 3 ? <p>{count} times</p> : <p>Fizz</p>}  
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
+    );
   }
   
-  if (num % 3 === 0) {
-    return "fizz";
-  }
-  
-  if (num % 5 === 0) {
-    return "buzz";
-  }  return num;
-
-  });
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
-}
 
 export {Ex3}
 
